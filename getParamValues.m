77f4359f3b2b270ad58fadd70af90b9439515c8a
nielsenlabmbi <nielsenlabmbi@gmail.com>
2018-12-10 14:14:11 -0500

    Add files via upload

--- getParamValues.m
+++ getParamValues.m
@@ -1,4 +1,4 @@
-function [MtNL,C50,N,W,WI,I,ContrastSat,ExcW,InhW] = getParamValues(IDs)
+function [MtNL,C50,N,W,WI,I] = getParamValues(IDs)
 
 MtNL=[];
 C50=[];
@@ -57,25 +57,5 @@ WI(i) = WidthILower + double(WidthIId)*(WidthIUpper-WidthILower)/(Steps-1);
 MtNL(i) = MtNLLower + double(MtNLId)*MtNLUpper/(Steps-1);
 C50(i) = C50Lower + double(C50Id)*(C50Upper-C50Lower)/(Steps-1);
 N(i) = NLower + double(NId)*(NUpper-NLower)/(Steps-1);
-CRI(i) = NC50toCrossOriInh(NId,C50Id);
-
-ContrasRespFunc = (([0:0.01:1].^N(i))./([0:0.01:1].^N(i)+C50(i).^N(i)))/(1/(1+C50(i).^N(i)));
-
-MTE = circ_vmpdf([0:1:15]*(2*pi/16),14*pi/16,2^W(i));
-MTE = MTE./max(MTE);
-
-MTI = circshift(circ_vmpdf([0:1:15]*(2*pi/16),14*pi/16,2^WI(i)),8);
-MTI = MTI./max(MTI);
-
-MTW = MTE - MTI*I(i);
-
-ContrastSat(i) = ContrasRespFunc(51);
-PosMTW = MTW;
-PosMTW(PosMTW<0) = 0;
-NegMTW = MTW;
-NegMTW(NegMTW>0) = 0;
-
-ExcW(i) = sum(PosMTW > 0.2*max(PosMTW));
-InhW(i) = sum(-NegMTW > 0.2*max(PosMTW));
 
 end
