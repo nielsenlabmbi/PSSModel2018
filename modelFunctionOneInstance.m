8bc7705c320394af145cd5cfdc2431bb1fe26f18
nielsenlabmbi <nielsenlabmbi@gmail.com>
2018-12-10 14:37:34 -0500

    Update modelFunctionOneInstance.m

--- modelFunctionOneInstance.m
+++ modelFunctionOneInstance.m
@@ -4,8 +4,8 @@ function MtModel = modelFunctionOneInstance(JobID)
 
 Steps = 10;
 
-MtNLLower = 0;
-MtNLUpper = 0.7;
+PSSNLLower = 0;
+PSSNLUpper = 0.7;
 
 C50Lower = 0.01;
 C50Upper = 1;
@@ -35,7 +35,7 @@ ID = mod(ID,10000);
 WidthIId = idivide(ID,Steps^3);
 ID = mod(ID,1000);
 
-MtNLId = idivide(ID,Steps^2);
+PSSNLId = idivide(ID,Steps^2);
 ID = mod(ID,100);
 
 NId = idivide(ID,Steps^1);
@@ -46,7 +46,7 @@ C50Id = ID;
 Inh = InhLower + double(InhId)*InhUpper/(Steps-1);
 Width = WidthLower + double(WidthId)*(WidthUpper-WidthLower)/(Steps-1);
 WidthI = WidthILower + double(WidthIId)*(WidthIUpper-WidthILower)/(Steps-1);
-MtNL = MtNLLower + double(MtNLId)*MtNLUpper/(Steps-1);
+PSSNL = PSSNLLower + double(PSSNLId)*PSSNLUpper/(Steps-1);
 
 % Get V1 Responses from premade LGN responses
 
@@ -62,37 +62,39 @@ for N = 1:16
     popV1Resp(:,:,N) = circshift(V1Resp,[N-1 N-1]);
 end
 
-% Set MT Weights
+% Set PSS Weights
 
-MTE = circ_vmpdf([0:1:15]*(2*pi/16),14*pi/16,2^Width);
-MTE = MTE./max(MTE);
+PSSE = circ_vmpdf([0:1:15]*(2*pi/16),14*pi/16,2^Width);
+PSSE = PSSE./max(PSSE);
 
-MTI = circshift(circ_vmpdf([0:1:15]*(2*pi/16),14*pi/16,2^WidthI),8);
-MTI = MTI./max(MTI);
+PSSI = circshift(circ_vmpdf([0:1:15]*(2*pi/16),14*pi/16,2^WidthI),8);
+PSSI = PSSI./max(PSSI);
 
-MTW = MTE - MTI*Inh;
+PSSW = PSSE - PSSI*Inh;
 
-% Get MT Resp
+% Get PSS Resp
 
-MtModel = zeros(16,16);
+PSSModel = zeros(16,16);
  
 for i = 1:16
-    MtModel = MtModel + popV1Resp(:,:,i).*MTW(i);
+    PSSModel = PSSModel + popV1Resp(:,:,i).*PSSW(i);
 end
 
-if max(MtModel(:))>0
-MtModel=MtModel./(max(MtModel(:)));
+if max(PSSModel(:))>0
+PSSModel=PSSModel./(max(PSSModel(:)));
 else
-MtModel = zeros(16,16);    
+PSSModel = zeros(16,16);    
 end
 
-MtModel = MtModel - MtNL;
-MtModel(MtModel<0)=0;
+PSSModel = PSSModel - PSSNL;
+PSSModel(PSSModel<0)=0;
+
+% Allign to max ori
 
 for ori = 1:16
-TCunit(ori) = MtModel(ori,ori);
+TCunit(ori) = PSSModel(ori,ori);
 end
 
 mx = find(TCunit == max(TCunit));
 
-MtModel = MtModel([mx:end 1:mx-1],[mx:end 1:mx-1]);
+PSSModel = PSSModel([mx:end 1:mx-1],[mx:end 1:mx-1]);
